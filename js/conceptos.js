e7905b6e46543b38ec2e022ddecd308fd34fa163
ositamylee <orgs34@gmail.com>
2022-11-17 14:06:56 -0500

    vjs17-11-2022

---
+++ js/conceptos.js
@@ -0,0 +1,16 @@
+// conceptos basicos en javascript
+/* 
+declarar variables en java script:
+1. var
+2. let
+3. const
+*/ 
+
+var nombre = "omar"; // variable de tipo cadena de texto
+let edad = 38; // variable de tipo entero
+const iva = 0.19; // variable de tipo flotante
+
+// datos de salida o imprimir un dato en la paguina web
+document.write(nombre);
+// console.log en la consola del navegadorn o devtools
+console.log(nombre);
