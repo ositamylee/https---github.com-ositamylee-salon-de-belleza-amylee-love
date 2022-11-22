179d0b8962cd65fa908000f7e369eda7be99d952
ositamylee <orgs34@gmail.com>
2022-11-22 14:05:05 -0500

    v22-11-2022

---
+++ js/factura.js
@@ -0,0 +1,7 @@
+// crear una funcion
+function calcular(){
+    // definir una variables para el iva precio y cantidad
+    const iva =0.19;
+    let precio = document.simuladorfactura.precio.value;
+
+}
